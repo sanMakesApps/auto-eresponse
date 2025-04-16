6591bd67b75016abcea2319de170d07342f98700
Tskar <sanskarthapaofficial@gmail.com>
2025-04-16 03:17:20 -0400

    Frontend implementation and project screenshots for Documentation

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['js', 'jsx'],
+};
